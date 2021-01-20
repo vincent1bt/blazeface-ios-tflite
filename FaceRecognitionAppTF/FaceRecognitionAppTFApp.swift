6f6c1446ba92afd4085426933cad61eec2be3d05
vincent1bt <vincent14319@gmail.com>
2021-01-19 19:26:37 -0600

    Initial Commit

---
+++ FaceRecognitionAppTF/FaceRecognitionAppTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionAppTFApp.swift
+//  FaceRecognitionAppTF
+//
+//  Created by vicente rodriguez on 19/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecognitionAppTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
